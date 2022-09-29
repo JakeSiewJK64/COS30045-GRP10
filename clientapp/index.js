ad4d6f409568102ee7325c00e896e0b1c41b8c55
DESKTOP-TN9N4CC\Think <joekanesiew@outlook.com>
2022-09-29 13:09:28 +0800

    added intro animation

--- clientapp/index.js
+++ clientapp/index.js
@@ -1,5 +1,19 @@
 window.onload = () => {
 
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+            else {
+                entry.target.classList.remove("show");
+            }
+        })
+    })
+
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
+
     const initializeLinegraph = (myDataset) => {
 
         // set the dimensions and margins of the graph
